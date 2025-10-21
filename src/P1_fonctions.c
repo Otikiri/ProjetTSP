0250bba4ef5b7be7cfe1f121ac1abb70f3002e58
Oscar Puente <oscarpuente2705@gmail.com>
2025-10-21 16:19:34 +0200

    version de oscar de signal

--- src/P1_fonctions.c
+++ src/P1_fonctions.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
+#include <signal.h>
 
 #include "struct.h"
 #include "P1_fonctions.h"
@@ -90,11 +91,79 @@ int fill_tournee_from_perm(const Graphe* g, const int* perm, int n, Tournee* out
     return 0;
 }
 
+/**
+ * @brief Transforme une permutation (tableau d'entiers) en une string qui est passee en parametre
+ * @param perm  La permutation actuelle (qui est un tableau d'entiers)
+ * @param length Longueur de perm
+ * @param outString La chaine de caracteres dans laquelle on ecrit le resultat
+ * @return Void
+ */
+void permToString(int* perm, int length, char* outString){
+    if (!outString){
+        outString = NULL;
+        return;
+    }
+
+    outString[0] = '\0';
+    strcat(outString,"[");
+
+    for (int i = 0; i < length; i++){
+        char tmp[10];
+        sprintf(tmp, "%d", perm[i]);
+        strcat(outString, tmp);
+        if (i < length - 1){
+            strcat(outString, ",");
+        }
+    }
+    strcat(outString, "]");
+}
+
+
+
+/**
+ * @brief Fonction de gestion de signale, envoie les valeur trouves au moment du signal et appelle le handler du signal
+Prend en argument: 
+ * @param bestPerm la meilleur permutation rencontre*
+ * @param bestLen la longueur de cette permutation
+ * @param perm la permutation courrante 
+ * @param taille la taille des permutations 
+ * @return Void
+ */
+void gestionSignal(int* bestPerm, double bestLen, int* perm, int taille){
+    char bestPermString[512];
+    char permActuelle[512];
+    char  c;
+    
+    permToString(perm,taille,permActuelle);
+    permToString(bestPerm,taille,bestPermString);
+
+    printf("Vous avez appuye sur la touche Ctrl-C\n");
+    printf("Meilleure tournee:\n %s", bestPermString);
+    printf("Longueur de la meilleure tournee: %f\n", bestLen);
+    printf("Permutation actuelle: %s\n", permActuelle);
+
+    printf("Voulez vous vraiment quitter? [y or n]\n");
+     
+     c = getchar();
+     if (c == 'y' || c == 'Y'){
+       exit(0);
+     }
+     else{
+          printf("On reprends\n");
+     }
+     return;
+};
+
 int tsp_bruteforce(const Graphe* g, DistanceFun f, bool faire_matrice_distance , Tournee* outBest, double* outBestLen, Tournee* outWorst,double* outWorstLen) {
     if (!g || g->dimension <= 0 || !outBest || !outBestLen) return -1;
     const int N = g->dimension;
 
-    
+    /* initialisation signales*/
+    sigset_t set,pendingSet;
+    sigaddset(&set,SIGINT);
+
+    /*instalation du masque */
+    sigprocmask(SIG_SETMASK,&set,NULL);
 
     /* initialisation */
     int perm[N];
@@ -138,6 +207,9 @@ int tsp_bruteforce(const Graphe* g, DistanceFun f, bool faire_matrice_distance ,
             worstLen = L;
             memcpy(worstPerm, perm, (size_t)N * sizeof(int));
         }
+        /* gestion du signal */
+        sigpending(&pendingSet)
+        if (sigismember(&pendingSet,SIGINT)==1) gestionSignal(bestPerm,bestLen,perm,N);
     }
 
 
@@ -150,3 +222,4 @@ int tsp_bruteforce(const Graphe* g, DistanceFun f, bool faire_matrice_distance ,
     *outWorstLen = worstLen;
     return 0;
 }
+
