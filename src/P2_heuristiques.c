51437c4ceb31530b7b3372a6589b9e71387e3a66
splat31 <bastien31800@orange.fr>
2025-10-29 16:16:25 +0100

    P2_heuristique: iter1 nearest neighbour
    Première itération de nearest neighbour sans test ni commentaires

--- src/P2_heuristiques.c
+++ src/P2_heuristiques.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include "calc_distance.h"
 
+bool isIn(int n, int* tab, int tabLen);
+
 int tsp_randomWalk(const Graphe* graphe, DistanceFun f, Tournee* outTournee, double* outLen){
     if (!graphe || graphe->dimension <= 0 || !outTournee || !outLen) return -1;
     // Initialisation du randomizer en utilisant le moment actuel en tant que seed
@@ -27,6 +29,37 @@ int tsp_randomWalk(const Graphe* graphe, DistanceFun f, Tournee* outTournee, dou
         outTournee->ord_point_vis[i] = graphe->point[randomIndice];
     }
 
+    *outLen = calcul_poids_tournee(outTournee, f);
+    free(tabOldIndice);
+    return 0;
+}
+
+int tsp_nearestNeighbour(const Graphe* graphe, DistanceFun f, Tournee* outTournee, double* outLen) {
+    if (!graphe || graphe->dimension <= 0 || !outTournee || !outLen) return -1;
+    outTournee->ord_point_vis[0]=graphe->point[0];
+
+    int* tabOldIndice = malloc(sizeof(int) * graphe->dimension);
+    tabOldIndice[0] = 0;
+    int nbOldIndice=1;
+    int lastIndice =0;
+    double distance_min=-1;
+    int fin=-1;
+
+    for (int j=1;j<graphe->dimension;j++) {
+        for (int i =1;i<graphe->dimension;i++) {
+            if (!isIn(i, tabOldIndice, nbOldIndice)){
+                double distance_temp=f(graphe->point[lastIndice],graphe->point[i]);
+                if (distance_min==-1||distance_temp<distance_min) {
+                    distance_min=distance_temp;
+                    fin=i;
+                }
+            }
+        }
+        tabOldIndice[nbOldIndice]=fin;
+        nbOldIndice++;
+        lastIndice=fin;
+        outTournee->ord_point_vis[nbOldIndice]=graphe->point[fin];
+    }
     *outLen = calcul_poids_tournee(outTournee, f);
     free(tabOldIndice);
     return 0;
